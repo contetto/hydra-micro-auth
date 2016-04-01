b21c634dbcc9742d9ffc42149eba4555f406cf96
Long Hoang <long@mindworker.de>
2016-04-01 19:43:06 +0800

    Fix imports and return statements

--- auth.go
+++ auth.go
@@ -1,6 +1,9 @@
 package hydraauth
 
-import "golang.org/x/net/context"
+import (
+	"github.com/micro/go-platform/auth"
+	"golang.org/x/net/context"
+)
 
 // This package is implementing following interface of micro-platform/auth:
 /*
@@ -36,63 +39,69 @@ type Auth interface {
 }
 */
 
-type Auth struct{}
+type Auth struct {
+	BaseURL string
+}
+
+func New(baseURL string) Auth {
+	return Auth{BaseURL: baseURL}
+}
 
 // Determine if a request with context is authorised
 // Should extract token from the context, check with
 // the authorizer and return an err if not authed.
 // Can be used for both client and server
-func (a *Auth) Authorized(ctx context.Context, req Request) (*Token, error) {
-
+func (a Auth) Authorized(ctx context.Context, req auth.Request) (*auth.Token, error) {
+	return &auth.Token{}, nil
 }
 
 // Retrieve a token for this client, should handle refreshing
-func (a *Auth) Token() (*Token, error) {
-
+func (a Auth) Token() (*auth.Token, error) {
+	return &auth.Token{}, nil
 }
 
 // Lookup a token
-func (a *Auth) Introspect(ctx context.Context) (*Token, error) {
-
+func (a Auth) Introspect(ctx context.Context) (*auth.Token, error) {
+	return &auth.Token{}, nil
 }
 
 // Revoke a token
-func (a *Auth) Revoke(t *Token) error {
-
+func (a Auth) Revoke(t *auth.Token) error {
+	return nil
 }
 
 // Will retrieve token from the context
-func (a *Auth) FromContext(ctx context.Context) (*Token, bool) {
-
+func (a Auth) FromContext(ctx context.Context) (*auth.Token, bool) {
+	return &auth.Token{}, true
 }
 
 // Creates a context with the token which can be
-func (a *Auth) NewContext(ctx context.Context, t *Token) context.Context {
-
+func (a Auth) NewContext(ctx context.Context, t *auth.Token) context.Context {
+	return ctx
 }
 
 // Retrieves token from headers
 // We may get back a partial token here{
-
-func (a *Auth) FromHeader(map[string]string) (*Token, bool) {
-
+func (a Auth) FromHeader(map[string]string) (*auth.Token, bool) {
+	return &auth.Token{}, true
 }
 
 // Adds token to headers
-func (a *Auth) NewHeader(map[string]string, *Token) map[string]string {
-
+func (a Auth) NewHeader(map[string]string, *auth.Token) map[string]string {
+	stringMap := make(map[string]string)
+	return stringMap
 }
 
 // We cache policies locally from the auth server
-func (a *Auth) Start() error {
-
+func (a Auth) Start() error {
+	return nil
 }
 
-func (a *Auth) Stop() error {
-
+func (a Auth) Stop() error {
+	return nil
 }
 
 // Name
-func (a *Auth) String() string {
-
+func (a Auth) String() string {
+	return ""
 }
